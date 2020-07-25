f4d788b18d321fddd1c89d1ec31196b85e54ead8
shihongxu <761228594@qq.com>
2020-07-25 11:46:12 +0800

    mod

--- config-server/src/main/java/com/dn/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/dn/configserver/ConfigServerApplication.java
@@ -1,8 +1,9 @@
-package com.axu.configserver;
+package com.dn.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -10,6 +11,7 @@ public class ConfigServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
+//        System.out.println(System.getProperty("user.dir"));
     }
 
 }
