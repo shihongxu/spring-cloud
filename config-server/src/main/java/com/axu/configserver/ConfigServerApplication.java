7e171a8bccbfc0156f210a28df771574f86d2b71
shihongxu <761228594@qq.com>
2020-07-25 12:52:15 +0800

    修改

--- config-server/src/main/java/com/axu/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/axu/configserver/ConfigServerApplication.java
@@ -10,7 +10,6 @@ public class ConfigServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
-//        System.out.println(System.getProperty("user.dir"));
     }
 
 }
